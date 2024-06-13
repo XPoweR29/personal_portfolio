671a9357789eec5727e6d736bab4c9f0deb24a2d
XPoweR29 <xpower-29@wp.pl>
2024-06-13 07:11:37 +0200

    Created double site container and implement proper logic

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -1,16 +1,35 @@
-"use client"
-
-import { ReactNode } from "react";
+"use client";
+import { usePathname } from "next/navigation";
+import { ReactNode, useEffect, useState } from "react";
 
 interface Props {
-    className?: string;
-    children: ReactNode;
-}
+	className?: string;
+	children: ReactNode;
+};
 
-export const MainContent = ({className, children}: Props) => {
-    return (
-        <main className={className}>
-            {children}
-        </main>
-    );
-}
+export const MainContent = ({ className, children: page }: Props) => {
+	const pathname = usePathname();
+	const [changingPage, setChangingPage] = useState(false);
+
+	useEffect(() => {
+		setChangingPage(true);
+		const timer = setTimeout(() => {
+			setChangingPage(false);
+
+		}, 700);
+
+		return () => clearTimeout(timer);
+	}, [pathname]);
+
+	return (
+		<main className={className}>
+			<main className={className}>
+				{changingPage ? (
+					<div className={`${"page"} ${"contentEnter"}`}>{page}</div>
+				) : (
+					<div className={"page"}>{changingPage ? null : page}</div>
+				)}
+			</main>
+		</main>
+	);
+};
