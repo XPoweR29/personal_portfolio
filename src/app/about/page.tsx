f7cfb27ed6e67774820a0ff7bb3e9aa0610fda3f
XPoweR29 <xpower-29@wp.pl>
2024-07-02 18:08:57 +0200

    Created About sbpage

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,29 +1,60 @@
+"use client"
+
 import Wrapper from "@/components/Wrapper/Wrapper";
+import { Icon } from "@iconify/react/dist/iconify.js";
 import styles from "./About.module.scss";
+import waves from "../../../public/assets/waves.png";
+import photo_2 from "../../../public/assets/photo_2.png";
+import { rubikGlitch } from "../fonts";
+import { useBreakpoints } from "@/hooks/useBreakpoints";
+import { MainBtn } from "@/components/MainBtn/MainBtn";
 
 const About = () => {
+	const {breakpoint} = useBreakpoints();
+
 	return (
 		<section className={styles.about}>
-			<Wrapper>
-				<h1>About my person...</h1>
-				<hr />
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi velit
-					mollitia itaque. Nostrum tempore cumque laborum saepe soluta velit
-					temporibus ipsam, non vel voluptatem similique repellat magni illum
-					voluptatum harum, nulla iusto architecto. Deleniti enim odio
-					praesentium ipsa at necessitatibus quia dignissimos accusantium
-					numquam? Saepe id eius porro asperiores, fugiat, a non eos dolores
-					quibusdam voluptatibus quisquam unde, est sed aliquid recusandae quos
-					libero praesentium ea! Nobis soluta consequuntur corrupti voluptates
-					suscipit praesentium accusamus a ratione beatae non rerum cupiditate,
-					architecto temporibus fuga sequi labore saepe alias officiis
-					perspiciatis! Dolore distinctio, reiciendis molestias officia alias
-					recusandae exercitationem enim cum sapiente ipsa beatae aspernatur
-					minus nam iste? Distinctio aliquid voluptatem deserunt nam voluptate
-					esse vitae necessitatibus possimus dicta, accusamus obcaecati facere
-					inventore non molestias?
+			<img className={styles.wavesImg} src={waves.src} alt="" />
+			{breakpoint.md && (
+				<img className={styles.photo} src={photo_2.src} alt="" />
+			)}
+			<Wrapper className={styles.wrapper}>
+				<h1 className={styles.heading}>
+					<span className={rubikGlitch.className}>A</span>bout me
+				</h1>
+				<p className={styles.text}>
+					Hi, I'm Paweł, and I'm an aspiring web developer with a passion for
+					programming. I have a solid foundation in various technologies and
+					have gained practical skills by developing applications and websites.
+					I have experience in teamwork and communication with clients. I'm
+					taking my first steps in freelancing and looking for a challenge in
+					the dynamic world of IT, where I can develop my skills and passion.
 				</p>
+
+				{breakpoint.md && <MainBtn filled className={styles.cvBtn}>Download CV</MainBtn>}
+
+				<div className={styles.techStack}>
+					<h2 className={styles.title}>Tech Stack</h2>
+					<p className={styles.text}>
+						These are the tools and technologies I enjoy working with to build
+						my web projects:
+					</p>
+
+					<div className={styles.container}>
+						<Icon icon="devicon:html5" />
+						<Icon icon="vscode-icons:file-type-css" />
+						<Icon icon="devicon:javascript" />
+						<Icon icon="vscode-icons:file-type-sass" />
+						<Icon icon="devicon:typescript" />
+						<Icon icon="devicon:react-wordmark" />
+						<Icon icon="devicon:nodejs-wordmark" />
+						<Icon icon="skill-icons:expressjs-dark" />
+						<Icon icon="devicon:mysql-wordmark" />
+						<Icon icon="devicon:mongodb-wordmark" />
+						<Icon icon="devicon:figma" />
+						<Icon icon="devicon:git" />
+					</div>
+				</div>
 			</Wrapper>
 		</section>
 	);
