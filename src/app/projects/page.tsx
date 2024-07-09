4a12895f4979e4edce123df255ef93be60762dd0
XPoweR29 <xpower-29@wp.pl>
2024-07-09 17:23:38 +0200

    Completed Projects subpage with RWD

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,12 +1,62 @@
+"use client";
 
 import Wrapper from "@/components/Wrapper/Wrapper";
 import styles from "./Projects.module.scss";
+import atom from "../../../public/assets/projects_atom.webp";
+import adona_mockup from "../../../public/assets/mockup_adona.png";
+import kwatery_mockup from "../../../public/assets/mockup_kwatery.png";
+import portfolio_mockup from "../../../public/assets/mockup_portfolio.png";
+import { rubikGlitch } from "../fonts";
+import { ProjectCard } from "@/components/ProjectCard/ProjectCard";
 
 const Projects = () => {
+
 	return (
-		<Wrapper className={styles.wrapper}>
-			<h1>Here is my projects!</h1>
-		</Wrapper>
+		<section className={styles.projects}>
+			<Wrapper className={styles.wrapper}>
+				<img src={atom.src} alt="" className={styles.atom}/>
+				<img src={atom.src} alt="" className={styles.atom}/>
+
+				<h1 className={styles.heading}>
+					<span className={rubikGlitch.className}>P</span>rojects
+				</h1>
+
+				<p className={styles.text}>
+					You can explore the various web development projects I have worked on,
+					showcasing my skills and experience in creating functional and
+					visually appealing web applications.
+				</p>
+
+				<div className={styles.cardsContainer}>
+					<ProjectCard
+						mockupImg={adona_mockup.src}
+						title="Orientl Restaurnat Website"
+						techStack="React | TypeScript | Firebase | Sass"
+						details="It is a collaborative project with a real client, featuring an admin panel, Firebase-based authentication, and robust form handling with validation."
+						demoUrl="http://www.adona.net.pl/"
+						gitHubUrl="https://github.com/XPoweR29/adona-oriental/tree/main"
+					/>
+
+					<ProjectCard
+						mockupImg={kwatery_mockup.src}
+						title="Room Rental Website"
+						techStack="React | TypeScript | Sass"
+						details="A real project developed for a client. The website includes multiple subpages, handles form submissions, and is optimized for key search terms to enhance visibility."
+						demoUrl="https://www.kwateryuzosi.pl/"
+						gitHubUrl="https://github.com/XPoweR29/beskidzka-kwatera"
+					/>
+
+					<ProjectCard
+						mockupImg={portfolio_mockup.src}
+						title="Web Developer Personal Portfolio"
+						techStack="NextJS | TypeScript | Sass"
+						details="My Personal Portfolio website, built using NextJS showcases my projects and CV. Designed to highlight my skills and experience. For development efficiency and code quality, I also integrated Husky for pre-commit hooks."
+						demoUrl="#"
+						gitHubUrl="https://github.com/XPoweR29/personal_portfolio"
+					/>
+				</div>
+			</Wrapper>
+		</section>
 	);
 };
 
---
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -0,0 +1,50 @@
+import { Icon } from "@iconify/react/dist/iconify.js";
+import background from "../../../public/assets/background.png";
+import { ThemeContext } from "@/context/ThemeContext";
+import { useContext } from "react";
+import styles from "./ProjectCard.module.scss";
+
+interface Props {
+    mockupImg: string;
+    title: string;
+    techStack: string;
+    details: string;
+    demoUrl: string;
+    gitHubUrl: string;
+};
+
+export const ProjectCard = ({mockupImg, title, techStack, details, demoUrl, gitHubUrl}: Props) => {
+	const { theme } = useContext(ThemeContext)!;
+
+	return (
+		<div className={styles.card}>
+			<img
+				src={background.src}
+				alt=""
+				className={styles.background}
+				style={{ filter: theme === "dark" ? "invert(1)" : "none" }}
+			/>
+			<img
+				src={mockupImg}
+				alt="Miniatura projektu"
+				className={styles.thumbnail}
+			/>
+			<h2 className={styles.title}>{title}</h2>
+			<p className={styles.tech}>{techStack}</p>
+
+			<div className={styles.details}>
+				<h2>{techStack}</h2>
+				<p>{details}</p>
+
+				<div className={styles.links}>
+					<a href={demoUrl} className={styles.demo} target="_blank" rel="noopener noreferrer">
+						<Icon icon="mynaui:arrow-up" />
+					</a>
+					<a href={gitHubUrl} className={styles.github} target="_blank" rel="noopener noreferrer">
+						<Icon icon="mingcute:github-fill" />
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+};
