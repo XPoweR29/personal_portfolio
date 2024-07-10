23a8f0227f4be7cdb72af5af2b3137ad7b5c871e
XPoweR29 <xpower-29@wp.pl>
2024-07-10 18:30:15 +0200

    Created Contact subpage

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,15 +1,67 @@
 import Wrapper from "@/components/Wrapper/Wrapper";
+import styles from "./Contact.module.scss";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import { rubikGlitch } from "../fonts";
+import { Form } from "@/components/Form/Form";
+import net from "../../../public/assets/net.webp";
 
 const Contact = () => {
 	return (
-		<Wrapper>
-			<h1>Contact Me!</h1>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam neque eos
-				recusandae magnam aut excepturi officiis voluptates nulla ut
-				repellendus.
-			</p>
-		</Wrapper>
+		<section className={styles.contact}>
+			<img src={net.src} alt="" className={styles.netImg} />
+
+			<Wrapper className={styles.wrapper}>
+				<div className={styles.contentContainer}>
+					<h1 className={styles.heading}>
+						<span className={rubikGlitch.className}>C</span>ontact
+					</h1>
+					<h2 className={styles.title}>Reach Out Anytime!</h2>
+					<p className={styles.text}>
+						Thank you for visiting my portfolio page! I am grateful for your
+						time and interest in my projects. If you are looking for an
+						ambitious and fast learning web developer who wants to develop his
+						skills in a dynamic IT environment, I will be very happy if you give
+						me a chance. I encourage you to contact me via the links below or
+						the site form. I look forward to working with you!
+					</p>
+
+					<div className={styles.linkBox}>
+						<a href="tel:+48505264517" className={styles.link}>
+							<Icon icon="fluent:phone-24-filled" className={styles.icon} />
+							<span>+48 505-265-517</span>
+						</a>
+
+						<a href="mailto:pawelzydek29@gmail.com" className={styles.link}>
+							<Icon
+								icon="material-symbols:mail-sharp"
+								className={styles.icon}
+							/>
+							<span>pawelzydek29@gmail.com</span>
+						</a>
+
+						<a
+							href="https://www.linkedin.com/in/pawe%C5%82-%C5%BCydek-36a1b1306/"
+							className={styles.link}
+							target="_blank"
+							rel="noopener noreferrer">
+							<Icon icon="mdi:linkedin" className={styles.icon} />
+							<span>linkedin.com/in/paweł-żydek</span>
+						</a>
+
+						<a
+							href="https://github.com/XPoweR29"
+							className={styles.link}
+							target="_blank"
+							rel="noopener noreferrer">
+							<Icon icon="mingcute:github-fill" className={styles.icon} />
+							<span>github.com/XPoweR29</span>
+						</a>
+					</div>
+				</div>
+
+				<Form />
+			</Wrapper>
+		</section>
 	);
 };
 
--- src/components/MainBtn/MainBtn.tsx
+++ src/components/MainBtn/MainBtn.tsx
@@ -2,14 +2,15 @@ import { ReactNode } from "react";
 import styles from "./MainBtn.module.scss";
 
 interface Props {
-    children: ReactNode;
-    filled?: boolean;
-    className?: string;
+	children: ReactNode;
+	filled?: boolean;
+	className?: string;
+	type?: "submit" | "button" | "reset" | undefined;
 }
 
-export const MainBtn = ({children, filled, className}: Props) => {
+export const MainBtn = ({children, filled, className, type}: Props) => {
     return (
-        <button className={`${className} ${styles.btn} ${filled?styles["btn--filled"]:""}`}>
+        <button className={`${className} ${styles.btn} ${filled?styles["btn--filled"]:""}`} type={type}>
             {children}
         </button>
     );
